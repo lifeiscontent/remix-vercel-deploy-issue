7c7a14f8bed6a1f849237bfcd37d6a161c3626fa
Aaron Reisman <aaronreisman@sort.xyz>
2024-03-07 11:17:36 -0800

    bug: remix vercel deploy issue

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import collaborationUrl from "~/assets/homepage/collaboration.png?url";
+import { imageUrl } from "~/utils/image";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +9,12 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export default function Index() {
+export default function Route() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <img
+      alt="Collaboration"
+      src={imageUrl(collaborationUrl, 606)}
+      width={606}
+    />
   );
 }
---
+++ app/utils/image.ts
@@ -0,0 +1,9 @@
+export function imageUrl(url: string, width: number, quality = 75) {
+  const searchParams = new URLSearchParams({
+    url,
+    w: width.toString(),
+    q: quality.toString(),
+  });
+
+  return ENV.VERCEL ? `/_vercel/image?${searchParams.toString()}` : url;
+}
